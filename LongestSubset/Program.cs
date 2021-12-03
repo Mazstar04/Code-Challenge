3de883bc04e124474fe28351e736643d821a69e4
Mazstar04 <mazeedahhamzat@gmail.com>
2021-12-03 13:34:58 +0100

    init

---
+++ LongestSubset/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LongestSubset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(LongestSubset(new int[] { 19, 10,  12, 24, 10,  25, 22 }, 4));
+        }
+
+        public static int LongestSubset(int[] s, int k)
+        {
+            List<int> subset = s.Distinct().ToList(); 
+            
+            
+            for (int i = 0; i < subset.Count; i++)
+            {
+
+                for (int j = i+1; j < subset.Count; j++)
+                {
+
+                    if ((subset[i] + subset[j]) % k == 0)
+                    {
+                        subset.Remove(subset[j]);
+                    }
+                }
+            }
+           
+            return subset.Count();
+        }
+    }
+
+}
